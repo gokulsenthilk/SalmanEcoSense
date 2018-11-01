313b3379b90ac4d4dab84f11810a99b30e1a2aa8
unknown <gokulsenthilkuppusamy@gmail.com>
2018-11-01 08:13:21 +0530

    Changed categories and suppliers section

--- src/app/components/suppliers/suppliers.component.ts
+++ src/app/components/suppliers/suppliers.component.ts
@@ -9,20 +9,12 @@ declare var $ :any;
   styleUrls: ['./suppliers.component.css']
 })
 export class SuppliersComponent implements OnInit {
-  suppliers;
+  services = 
+  [{"id":"9","Service_Description":"Home cleaning","Status":"1"},{"id":"8","Service_Description":"Furniture assembly","Status":"1"},{"id":"7","Service_Description":"TV Mounting","Status":"1"},{"id":"6","Service_Description":"Interior painting","Status":"1"},{"id":"5","Service_Description":"Hanging pictures and shelves","Status":"1"}];;
   constructor(public http: HttpClient) { }
 
   ngOnInit() {
     setTimeout(this.InitializeCarousal,1000);
-    let responseData;
-    this.http.get('http://gokulsenthil.com/api/product/readsuppliers.php?authkey=ecosense')
-    .subscribe(data => {
-      responseData = data;
-      responseData = responseData.records;
-      this.suppliers = responseData;
-    setTimeout(this.InitializeCarousal,1000);
-      console.log(this.suppliers);
-    })
   }
   InitializeCarousal() {
     $("#owl-suppliers").owlCarousel({      
@@ -40,17 +32,16 @@ export class SuppliersComponent implements OnInit {
       navText: ['<i class="ui-arrow-left">','<i class="ui-arrow-right">'],
       responsive:{
         1200: {
-          items:3
+          items:4
         },
         768:{
-          items:2
+          items:3
         },
         540:{
           items:1
         }
       }
     })
-  
 }
 
 }
